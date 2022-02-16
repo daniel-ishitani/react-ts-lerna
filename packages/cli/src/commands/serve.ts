2c8e40c98b443f8ddac333177abbe8e29f7352c2
daniel-ishitani <ishitanidim@gmail.com>
2022-02-15 21:15:29 -0300

    adding local-only api

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -7,8 +7,19 @@ export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
-  .action((filename = 'notes.js', options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
+  .action(async (filename = 'notes.js', options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+  
+      await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(`Opened ${filename}. Navigate to localhost:${options.port} to edit.`);
+    } catch (err: any) {
+      if (err.code === 'EADDRINUSE') {
+        console.error('Port is in use. Try running on a different port. [-p <number>]');
+      } else {
+        console.log(err.message);
+      }
 
-    serve(parseInt(options.port), path.basename(filename), dir);
+      process.exit(1);
+    }
   });
