91bf82a7455422ac35c3f9bbc5cdb287a54b6611
daniel-ishitani <ishitanidim@gmail.com>
2022-02-17 19:58:41 -0300

    added local-only api

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -3,6 +3,8 @@ import { Command } from 'commander';
 
 import { serve } from 'local-api';
 
+const useProxy = !(process.env.NODE_ENV === 'production');
+
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
@@ -11,7 +13,7 @@ export const serveCommand = new Command()
     try {
       const dir = path.join(process.cwd(), path.dirname(filename));
   
-      await serve(parseInt(options.port), path.basename(filename), dir);
+      await serve(parseInt(options.port), path.basename(filename), dir, useProxy);
       console.log(`Opened ${filename}. Navigate to localhost:${options.port} to edit.`);
     } catch (err: any) {
       if (err.code === 'EADDRINUSE') {
