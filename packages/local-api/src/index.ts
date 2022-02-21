6ce1345c54737f5a75be91fa41e8dc2676d67e1d
daniel-ishitani <ishitanidim@gmail.com>
2022-02-21 19:04:19 -0300

    data persistence

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -6,6 +6,8 @@ import { createCellsRouter } from './routes/cells';
 
 export const serve = (port: number, filename: string, dir: string, useProxy: boolean) => {
   const app = express();
+
+  app.use(createCellsRouter(filename, dir));
   
   if (useProxy) {
     app.use(createProxyMiddleware({
@@ -17,8 +19,6 @@ export const serve = (port: number, filename: string, dir: string, useProxy: boo
     const packagePath = require.resolve('react-ts/build/index.html');
     app.use(express.static(path.dirname(packagePath)));
   }
-
-  app.use(createCellsRouter(filename, dir));
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
